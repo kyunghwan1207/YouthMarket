c24f6e9a9b34269ee90a040a5d3a35b2fe2428ef
kyunghwan1207 <gkw1207@hufs.ac.kr>
2022-07-23 01:28:32 +0900

    move files to front

--- youthmarket/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from users import views
-
-urlpatterns = [
-    path('login/', views.login, name="login"),
-    path('logout/', views.logout, name='logout'),
-    path('register/', views.register, name='register'),
-]
